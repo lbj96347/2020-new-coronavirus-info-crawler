fc65bb8df5c5fc0e2dc4477cae5689e3c322aad6
CashLee <cashlee96347@gmail.com>
2020-01-23 00:52:26 +0800

    [init]init info crawler

---
+++ city_info_crawler.js
@@ -0,0 +1,61 @@
+const request = require("request")
+const cityData = require('./cities.json')
+const parse = require("url-parse")
+const fs = require('fs');
+
+var count = 0;
+const newCityList = [];
+var singleCityPosition = [];
+
+const composeCitylist = function (){
+  const cityList = []; 
+  cityData["provinces"].map((item, key) => 
+    // cityList.push(item["citys"])
+    item["citys"].map((item, key) =>
+      cityList.push(item["citysName"])
+    )
+  )
+  return cityList;
+}
+// console.log( composeCitylist().length );
+
+const crawlerAction = function (cityName){
+  var requestUrl = parse('http://api.map.baidu.com/geocoder?key='+[YOUR_BAIDU_MAP_KEY]+'&address='+ cityName +'&output=json', true)
+  request(requestUrl.href, function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    if(!error){
+      console.log('body:', body); // Print the HTML for the Google homepage.
+      var cityPosition = JSON.parse(body)["result"]["location"]
+      console.log([cityPosition["lat"], cityPosition["lng"]]);
+      singleCityPosition = [cityPosition["lat"], cityPosition["lng"]]; 
+      return [cityPosition["lat"], cityPosition["lng"]]; 
+    }else{
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      return null
+    }
+  });
+}
+
+const requestQueue = function (index){
+  // console.log( allCities )
+  if( index < (allCities.length - 1) ){
+  // if( index < 5 ){
+    crawlerAction( allCities[index] )
+    newCityList.push({"city": allCities[index], "position": singleCityPosition})
+    count = count + 1
+    setTimeout(function (){
+      requestQueue(count)
+    }, 2000)
+  }else{
+    console.log( newCityList )
+    fs.writeFile("./city_position.json", JSON.stringify(newCityList), function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+    });
+  }
+}
+
+const allCities = composeCitylist() 
+requestQueue(0)
