dec9133d9379994e4d4e5fb81348e2949d54a01c
CashLee <cashlee96347@gmail.com>
2020-01-23 22:36:54 +0800

    [mod]get latest virus news url from news page

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const request = require("request")
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const healthCommissionUrls = require('./cities_health_commission_department_website.json')
 
-// "http://wsjkw.gd.gov.cn/zwyw_yqxx/content/post_2877905.html" 
+const keywords = ["新型冠状病毒", "通报", "情况", "病例", "疫情", "日期", "最新"];
 
-const crawlerAction = function (){
-  request("http://wsjkw.gd.gov.cn/zwyw_yqxx/index.html", function (error, response, body) {
+// Test URL "http://wsjkw.gd.gov.cn/zwyw_yqxx/content/post_2877905.html" 
+
+// Get into a specific local HC department news list and get original DOM info  
+const checkNewsList = function (url){
+  request(url, function (error, response, body) {
     console.error('error:', error); // Print the error if one occurred
     if(!error){
       // console.log('body:', body); // Print the HTML for the Google homepage.
       // console.log('final string :', finalString);
       const dom = new JSDOM(body);
-      console.log(dom.window.document.documentElement.textContent); // "Hello world"
+      checkLatestNewsUrl(dom.window.document);
+      //console.log(dom.window.document.documentElement.textContent); // "Hello world"
     }else{
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       return null
@@ -19,5 +24,46 @@ const crawlerAction = function (){
   });
 }
 
+// Extract the latest virus news and save the URL
+const checkLatestNewsUrl = function (doc){
+  var matchAnchors = []
+  for( var i in doc.getElementsByTagName("a") ){
+    var anchor = doc.getElementsByTagName("a")[i] 
+    var anchorText = anchor.innerHTML;
+    var keywordCount = 0;
+    if( anchorText){
+        keywords.map(function( word, key){
+            if(anchorText.search(word) > 0){
+                keywordCount = keywordCount + 1;
+            }
+        });
+        if( keywordCount >= 2 ){
+            console.log( anchorText );
+            matchAnchors.push(anchor)
+            // 有可能不具备年，月，日这个条件，但是已经是一个新闻
+            // 只需要取数据中，第一个元素的 href 即可
+            // href 中如果不具备 http 头的话，那么需要获取该 domain 下的 url 并补充回去
+            console.log( anchorText.match(/\b(年|月|日)\b/g) )
+        }
+    }
+  }
+  console.log( matchAnchors[0].href )
+}
 
-crawlerAction()
+const completeHref = function (href){
+
+}
+
+// Extract all local department HC news 
+const extractHCUrls = function (){
+  /*
+  healthCommissionUrls.map( function(item, key){
+    console.log(item["url"]);
+  });
+  */
+  checkNewsList(healthCommissionUrls[0]["url"])
+}
+
+extractHCUrls()
+
+// crawlerAction()
