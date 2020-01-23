cf415b111e549aa7b5bd73883af4524c19bbb347
CashLee <cashlee96347@gmail.com>
2020-01-23 21:09:37 +0800

    [mod]update city health commission department resources page link

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const request = require("request")
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
+// "http://wsjkw.gd.gov.cn/zwyw_yqxx/content/post_2877905.html" 
+
 const crawlerAction = function (){
-  request("http://wsjkw.gd.gov.cn/zwyw_yqxx/content/post_2877905.html", function (error, response, body) {
+  request("http://wsjkw.gd.gov.cn/zwyw_yqxx/index.html", function (error, response, body) {
     console.error('error:', error); // Print the error if one occurred
     if(!error){
       // console.log('body:', body); // Print the HTML for the Google homepage.
       // console.log('final string :', finalString);
       const dom = new JSDOM(body);
-      console.log(dom.window.document.documentElement.innerText); // "Hello world"
+      console.log(dom.window.document.documentElement.textContent); // "Hello world"
     }else{
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       return null
     }
   });
 }
+
 
 crawlerAction()
